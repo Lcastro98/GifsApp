fe11fe90ffef7df980ad7939fdbbba54cb7b7460
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-18 18:08:19 -0500

    Se organizan los datos para poder reutilizar y sea mas legible

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
@@ -8,6 +8,8 @@ import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 export class GifsService {
 
   private apiKey : string = 'i3ufdGtjId6HFXXUdeNy0qiZqOD9I1XZ';
+
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs'
 
   private _historial: string[] = [];
 
@@ -31,7 +33,12 @@ export class GifsService {
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=i3ufdGtjId6HFXXUdeNy0qiZqOD9I1XZ&q=${query}&limit=10`)
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`, {params})
       .subscribe( resp => {
         console.log(resp.data);
         this.resultados = resp.data;
