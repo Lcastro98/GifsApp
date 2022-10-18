e1ec2caed58626e26136e7cadc7ff02fcf836116
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-18 12:11:17 -0500

    Se almacenan las busquedas en el localstorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -17,13 +17,17 @@ export class GifsService {
     return [...this._historial]
   }
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient){
+    
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
 
   buscarGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
     if(!this._historial.includes(query)){
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=i3ufdGtjId6HFXXUdeNy0qiZqOD9I1XZ&q=${query}&limit=10`)
